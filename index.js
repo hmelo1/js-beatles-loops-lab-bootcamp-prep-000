01619fa428eca587921c8c31f4dd5e4216630c2e
hmelo1 <heriberto@melo.nyc>
2017-10-07 02:10:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function theBeatlesPlay(array1, array2){
   for (var i = 0; i < array1.length; i++){
     var array3 = []
-    array3.push(`${array1[i]} plays ${array2[i]}`)
+    array3.unshift(`${array1[i]} plays ${array2[i]}`)
   }
 }
